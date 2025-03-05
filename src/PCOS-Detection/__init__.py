edc68123a3a6e85914461814e2fae6e9e12f6ed5
Ullas <clubullas@gmail.com>
2025-03-05 14:22:45 +0530

    added notebook

---
+++ src/PCOS-Detection/__init__.py
@@ -0,0 +1,20 @@
+import sys
+import os
+import logging
+
+logging_str = "[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s"
+
+log_dir = "logs"
+log_filepath = os.path.join(log_dir, "PCOS-Detection.log")
+os.makedirs(log_dir, exist_ok=True)
+
+logging.basicConfig(
+    level = logging.INFO,
+    format = logging_str,
+    handlers = [
+        logging.FileHandler(log_filepath),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+logger = logging.getLogger("pcos_detection")
